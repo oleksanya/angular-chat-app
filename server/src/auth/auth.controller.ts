650be2ee14af736a8b4d88ffee4551ec38c6327e
Oleksandra  Vereshchagina <alexxa.vereshchagina@gmail.com>
2024-09-04 16:25:46 +0300

    set up authentication, created login and sign up routes

---
+++ server/src/auth/auth.controller.ts
@@ -0,0 +1,30 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignIn } from 'src/dto/user/signInDto.dto';
+import { AuthGuard } from './auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  async signIn(@Body() body: SignIn) {
+    return this.authService.signIn(body);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
---
+++ server/src/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(user: any): Promise<{ access_token: string }> {
+    const existUser = await this.userService.findByEmail(user.email);
+    const validateUser = await bcrypt.compare(
+      user.password,
+      existUser.password,
+    );
+
+    if (!validateUser) {
+      throw new UnauthorizedException();
+    }
+
+    const payload = { email: existUser.email, sub: existUser._id };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
