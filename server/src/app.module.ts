1f24ddf40798fa02fdda1f445c6bc2a90fb3a5ab
Oleksandra  Vereshchagina <alexxa.vereshchagina@gmail.com>
2024-09-04 16:26:18 +0300

    add connection with db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://oleksanya:3BV5lFjf14M7JXu7@cluster0.bckvy.mongodb.net/',
+      'mongodb+srv://oleksanya:3BV5lFjf14M7JXu7@cluster0.bckvy.mongodb.net/chat',
     ),
+    UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
