118798c6c6e483f76daa0609f7e6ae8d20911e69
Oleksandra Vereshchagina <alexxa.vereshchagina@gmail.com>
2024-09-08 17:09:21 +0300

    updated chat module

--- client/src/app/chat/chat.module.ts
+++ client/src/app/chat/chat.module.ts
@@ -1,12 +1,24 @@
-import { NgModule } from '@angular/core';
+import { importProvidersFrom, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChatRoutingModule } from './chat-routing.module';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { UserService } from '../services/user.service';
+import { authInterceptorProvider } from '../core/http/http.interceptor';
+import { ChatService } from '../services/chat.service';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ChatRoutingModule
+  ],
+  providers: [
+    UserService,
+    ChatService,
+    provideHttpClient(withInterceptorsFromDi()),
+    authInterceptorProvider
   ]
 })
 export class ChatModule { }
