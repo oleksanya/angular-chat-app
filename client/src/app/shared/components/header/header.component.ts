b425e091fd2b1933077676aad64d0260270bb30b
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2025-06-12 23:30:26 +0300

    feat: refactor header and footer components for improved readability and error handling

--- client/src/app/shared/components/header/header.component.ts
+++ client/src/app/shared/components/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
+import { Component, inject, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,13 +7,23 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [MatIconModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  constructor(private router: Router) {}
+  private router = inject(Router);
+
+  isLoggingOut = signal<boolean>(false);
 
   logOut(): void {
-    localStorage.clear();
-    this.router.navigate(['/auth']);
+    this.isLoggingOut.set(true);
+
+    try {
+      localStorage.clear();
+      this.router.navigate(['/auth']);
+    } catch {
+      console.error('Error during logout');
+    } finally {
+      this.isLoggingOut.set(false);
+    }
   }
 }
